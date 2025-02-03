9c3084c0c58fa8d90427f73d7b60ea3a55586950
anemoa <anemoia.yves@gmail.com>
2025-02-03 14:02:08 +0900

    feat: 기초 구조 수정 &  tailwind css 설치

--- src/ChallengeList/components/ChallengeListLayout.jsx
+++ src/ChallengeList/components/ChallengeListLayout.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const ChallengeListLayout = () => {
-  return (
-	<div>ChallengeListLayout</div>
-  )
-}
+    return (
+        <div className=''>
+            <div className='flex min-h-screen items-center justify-center'>
+                <h1 className='text-3xl font-bold text-blue-600'>
+                    Hello Tailwind!
+                </h1>
+            </div>
+        </div>
+    );
+};
 
-export default ChallengeListLayout
+export default ChallengeListLayout;
