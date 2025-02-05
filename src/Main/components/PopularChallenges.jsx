ed6df113072d5abc1bafa1647f2b0200f12345be
yu_hyeri <youhr02@gmail.com>
2025-02-05 17:22:03 +0900

    style: fontStyle변경

--- src/Main/components/PopularChallenges.jsx
+++ src/Main/components/PopularChallenges.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PopularChallenges = ({ challenges }) => {
     return (
         <>
-            <h2 className='text-xl font-bold mb-4'>인기 있는 챌린지</h2>
+            <h2 className='text-xl font-bold mb-3'>인기 있는 챌린지</h2>
             <div className='flex flex-col gap-4'>
                 {challenges.map((challenge, index) => (
                     <div key={index} className='bg-white p-4 rounded-xl'>
