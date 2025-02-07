77bf79157627f30914b87309faf29b730dfd64e1
jYoung15628 <jylee9802@naver.com>
2025-02-08 01:12:18 +0900

    feat: login logout 기능 추가 및 testEmail 값 추가

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 // import MyPage from '../MyPage/components/MyProfileSection';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Header = () => {
+
+    const [loggedInUser, setLoggedInUser] = useState(
+        localStorage.getItem('loggedInUser')
+    );
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const handleStorageChange = () => {
+            setLoggedInUser(localStorage.getItem('loggedInUser'));
+        };
+
+        window.addEventListener('storage', handleStorageChange);
+        return () => window.removeEventListener('storage', handleStorageChange);
+    }, []);
+
+    useEffect(() => {
+        setLoggedInUser(localStorage.getItem('loggedInUser'));
+    }, [loggedInUser]); 
+
+    const handleLogout = () => {
+        localStorage.removeItem('loggedInUser');
+        setLoggedInUser(null);
+        navigate('/');
+    };
+
     return (
         <header className='flex justify-between items-center w-[80%] max-w-[1344px] mx-auto h-20'>
             <Link to='main' className='h-10 bagel-fat-one-regular text-3xl'>
@@ -30,9 +56,13 @@ const Header = () => {
                         {/* <a href='/contact' className='hover:underline'>
                             로그인
                         </a> */}
-                        <Link to='/login' className='hover:underline'>
-                            로그인
-                        </Link>
+                        {loggedInUser ? (
+                            <button onClick={handleLogout}>로그아웃</button>
+                        ) : (
+                            <Link to='/login' className='hover:underline'>
+                                로그인
+                            </Link>
+                        )}
                     </li>
                 </ul>
             </nav>
