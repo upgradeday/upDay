a67dabc27526e76fd6cb9f0ff9c0f99d6ecf9dbc
yu_hyeri <youhr02@gmail.com>
2025-02-08 05:07:10 +0900

    Feat : 반응형 구조 수정, 버튼 추가

--- src/Main/components/CategoryList.jsx
+++ src/Main/components/CategoryList.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import CategoryItem from './CategoryItem';  // 새로 만든 CategoryItem 컴포넌트 import
+import CategoryItem from './CategoryItem'; // 새로 만든 CategoryItem 컴포넌트 import
 
 const CategoryList = ({ categories }) => {
     return (
-        <div className='mt-6'>
+        <div className='mt-5'>
             <div className='grid grid-cols-2 gap-4 mt-2'>
                 {categories.map((category, index) => (
                     <CategoryItem key={index} category={category} />
