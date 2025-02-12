fd2a0a2dec98f830e0247b1e7ed847dac05e26df
anemoa <anemoia.yves@gmail.com>
2025-02-12 21:30:43 +0900

    feat: 검색 기능 완료

--- src/ChallengeList/components/ChallengeListSearchSection.jsx
+++ src/ChallengeList/components/ChallengeListSearchSection.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { getChallenges } from '../../utils/localStorage';
 
 const btnList = [
     { title: '전체', color: '#121212' },
@@ -9,15 +10,51 @@ const btnList = [
     { title: '습관', color: '#FFDEE7' },
 ];
 
-const ChallengeListSearchSection = ({ setSelectedCategory }) => {
-	
-	const navigate = useNavigate();
+const ChallengeListSearchSection = ({
+    setSelectedCategory,
+    setSearchResults,
+}) => {
+    const navigate = useNavigate();
     const [activeCategory, setActiveCategory] = useState('전체');
+    const [searchTerm, setSearchTerm] = useState('');
 
     const handleCategoryClick = (category) => {
         setActiveCategory(category);
         setSelectedCategory(category); // 부모 컴포넌트에 선택한 카테고리 전달
-		navigate(`/challengelist/category/${category}`)
+        setSearchResults(null); // 카테고리 변경시 검색 결과 초기화
+        navigate(`/challengelist/category/${category}`);
+    };
+
+    // 검색 로직
+    const handleSearch = () => {
+
+        if (!searchTerm.trim()) {
+            setSearchResults(null); // 검색어가 없으면 검색 결과 초기화
+            return;
+        }
+
+		// 로컬 스토리지에 담긴 모든 챌린지 작성글
+        const challenges = getChallenges();
+
+		// 검색어 필터링 하는 로직
+        const results = challenges.filter(
+            (challenges) =>
+                challenges.title
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase()) ||
+                challenges.content
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase())
+        );
+
+        setSearchResults(results); // 검색 결과를 부모에게 전달
+    };
+
+    // Enter 키 눌러도 검색 가능
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleSearch();
+        }
     };
 
     return (
@@ -43,10 +80,16 @@ const ChallengeListSearchSection = ({ setSelectedCategory }) => {
             </ul>
             <input
                 type='text'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyPress={handleKeyPress}
                 placeholder='검색어를 입력하시오'
                 className='rounded-xl'
             />
-            <button className='px-8 py-[10px] rounded-xl bg-white'>
+            <button
+                onClick={handleSearch}
+                className='px-8 py-[10px] rounded-xl bg-white'
+            >
                 검색버튼
             </button>
         </section>
