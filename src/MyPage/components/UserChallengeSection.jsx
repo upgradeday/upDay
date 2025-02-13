b404b8c3de018cac38dd16316143fe77345ffe49
deeniee <enbn2704@gmail.com>
2025-02-13 19:58:36 +0900

    feat: 마이페이지 챌린지 목록 검색 및 필터링 기능 완성

--- src/MyPage/components/UserChallengeSection.jsx
+++ src/MyPage/components/UserChallengeSection.jsx
@@ -6,19 +6,27 @@ import UserChallengeList from './UserChallengeList';
 
 export default function UserChallengeSection() {
     const dispatch = useDispatch();
-    const joinedChallenges = useSelector(
-        (state) => state.myClgList.joinedChallenges
-    );
+    const joinedChallenges =
+        useSelector((state) => state.myClgList.joinedChallenges) || [];
     const [loggedInUser, setLoggedInUser] = useState(null);
     const [users, setUsers] = useState([]);
     const [isTestAccount, setIsTestAccount] = useState(true);
     const [categoryFilter, setCategoryFilter] = useState('전체');
     const [searchTerm, setSearchTerm] = useState('');
+    const [isMyPost, setIsMyPost] = useState('');
+    const [isDoingClg, setIsDoingClg] = useState('');
+    const [isDoneClg, setIsDoneClg] = useState('');
     const [filteredChallenges, setFilteredChallenges] = useState([]);
 
     useEffect(() => {
         dispatch(getMyJoinedChallenge());
     }, [dispatch]);
+
+    useEffect(() => {
+        if (joinedChallenges.length > 0) {
+            setFilteredChallenges([...joinedChallenges]);
+        }
+    }, [joinedChallenges]);
 
     // localStorage 값 가져올 때 예외 처리
     useEffect(() => {
@@ -40,7 +48,7 @@ export default function UserChallengeSection() {
         setIsTestAccount(users.length === 0 || loggedInUser === users[0].email);
     }, [users, loggedInUser]);
 
-    // 필터링된 챌린지 리스트 업데이트
+    // 검색에 따른 목록 노출
     useEffect(() => {
         let filtered = [...joinedChallenges];
 
@@ -55,29 +63,39 @@ export default function UserChallengeSection() {
             filtered = filtered.filter(
                 (challenge) =>
                     challenge.title.toLowerCase().includes(lowerSearchTerm) ||
-                    challenge.content.toLowerCase().includes(lowerSearchTerm) ||
-                    challenge.nickname.toLowerCase().includes(lowerSearchTerm)
+                    challenge.content.toLowerCase().includes(lowerSearchTerm)
             );
         }
 
-        setFilteredChallenges(filtered);
-    }, [joinedChallenges, categoryFilter, searchTerm]);
+        if (isMyPost) {
+            filtered = filtered.filter(
+                (challenge) => challenge.authorId === loggedInUser
+            );
+        }
 
-    // 검색 버튼 클릭 시 실행되는 함수
-    const handleSearch = () => {
-        setSearchTerm(searchTerm);
-    };
+        if (isDoingClg) {
+            filtered = filtered.filter((challenge) => challenge.clgDoing);
+        }
+
+        if (isDoneClg) {
+            filtered = filtered.filter((challenge) => challenge.clgDone);
+        }
+
+        // 결과가 없으면 빈 배열을 설정하여 "검색 결과 없음"을 표시
+        setFilteredChallenges(filtered.length > 0 ? filtered : []);
+    }, [
+        categoryFilter,
+        searchTerm,
+        joinedChallenges,
+        isMyPost,
+        isDoingClg,
+        isDoneClg,
+    ]);
 
     if (!isTestAccount) {
         return (
-            <div className='w-full h-[627px] md:h-[756px] rounded-r-3xl rounded-bl-3xl bg-neutral-100 p-[24px] md:p-[36px]'>
-                <UserChallengeSearch
-                // categoryFilter={categoryFilter}
-                // setCategoryFilter={setCategoryFilter}
-                // searchTerm={searchTerm}
-                // setSearchTerm={setSearchTerm}
-                // onSearch={handleSearch}
-                />
+            <div className='w-full h-[612px] md:h-[756px] rounded-r-3xl rounded-bl-3xl bg-neutral-100 px-[24px] py-[32px] md:p-[36px]'>
+                <UserChallengeSearch />
                 <p className='text-xs md:text-sm text-center text-gray-500 mt-4'>
                     테스트 계정이 아닌 경우, 해당 기능은 제한됩니다.
                 </p>
@@ -86,15 +104,20 @@ export default function UserChallengeSection() {
     }
 
     return (
-        <div className='w-full h-[627px] md:h-[756px] rounded-r-3xl rounded-bl-3xl bg-neutral-100 p-[24px] md:p-[36px]'>
+        <div className='w-full h-[612px] md:h-[756px] rounded-r-3xl rounded-bl-3xl bg-neutral-100 px-[24px] py-[32px] md:p-[36px]'>
             <UserChallengeSearch
                 categoryFilter={categoryFilter}
                 setCategoryFilter={setCategoryFilter}
                 searchTerm={searchTerm}
                 setSearchTerm={setSearchTerm}
-                onSearch={handleSearch}
+                isMyPost={isMyPost}
+                setIsMyPost={setIsMyPost}
+                isDoingClg={isDoingClg}
+                setIsDoingClg={setIsDoingClg}
+                isDoneClg={isDoneClg}
+                setIsDoneClg={setIsDoneClg}
             />
-            <UserChallengeList challenges={filteredChallenges} />
+            <UserChallengeList filteredChallenges={filteredChallenges} />
         </div>
     );
 }
